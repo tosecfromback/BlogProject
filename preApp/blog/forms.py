2376c0b104691a937cc1a2f86877741673693328
tosecformback <sub7503@naver.com>
2023-07-18 10:53:51 +0900

    가상환경에서 비정상적 구동으로 인해 프로젝트 초기화 및 blog기능 복구

--- preApp/blog/forms.py
+++ preApp/blog/forms.py
@@ -5,7 +5,7 @@ class PostForm(forms.ModelForm):
     
     class Meta:
         model = Post
-        fields = [ 'title', 'content', 'category']
+        fields = [ 'title', 'content' ]
 
 class CommentForm(forms.ModelForm):
     
--- preApp/blog/models.py
+++ preApp/blog/models.py
@@ -1,22 +1,22 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 
-User = get_user_model()
+# User = get_user_model()
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    category = models.CharField(max_length=30,default='')
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    # category = models.ForeignKey('Post', on_delete=models.SET_DEFAULT(None))
+    # writer = models.ForeignKey(None, on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
     content = models.TextField()
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    # writer = models.ForeignKey(None, on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -26,7 +26,7 @@ class Comment(models.Model):
 class HashTag(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    # writer = models.ForeignKey(None, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
